269d9b28bde786a079344f6f4755b78573e29655
Karol Programuje <karol341654@gmail.com>
2024-01-24 12:20:42 +0100

    initial commit

---
+++ src/test/java/kwgh0st/project/hotelbookingapp/HotelBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kwgh0st.project.hotelbookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
