70db7bee800777a077519c2c2a79e82cd9d235cb
Karol Programuje <karol341654@gmail.com>
2024-01-24 15:16:04 +0100

    Adding BookedRoom, adding Room classes

---
+++ src/main/java/kwgh0st/project/hotelbookingapp/model/BookedRoom.java
@@ -0,0 +1,54 @@
+package kwgh0st.project.hotelbookingapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookedRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+    @Column(name = "check_in")
+    private LocalDate checkInDate;
+    @Column(name = "check_out")
+    private LocalDate checkOutDate;
+    @Column(name = "guest_fullName")
+    private String guestFullName;
+    @Column(name = "guest_email")
+    private String guestEmail;
+    @Column(name = "adults")
+    private int numOfAdults;
+    @Column(name = "children")
+    private int numOfChildren;
+    @Column(name = "total_guest")
+    private int totalNumOfGuests;
+    @Column(name = "confirmation_code")
+    private String bookingConfirmationCode;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public void calculateTotalNumOfGuests() {
+        this.totalNumOfGuests = this.numOfAdults + this.numOfChildren;
+    }
+
+    public void setNumOfAdults(final int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        this.calculateTotalNumOfGuests();
+    }
+
+    public void setNumOfChildren(final int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        this.calculateTotalNumOfGuests();
+    }
+}
